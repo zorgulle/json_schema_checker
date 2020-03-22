8bb0925c4e000a266c8d122d08638c8b3968a1f4
Zorgulle <winy95battle@gmail.com>
2020-03-22 14:26:36 +0100

    Build it to the limit

---
+++ json_schema_checker/validator/validators.py
@@ -0,0 +1,3 @@
+class Test():
+    def toto(self):
+        print("Hello")
---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(name='json_schema_checker',
+      version='0.1.0',
+      description='Json format checker',
+      author='Zorgulle',
+      author_email='winy95battle@gmail.com',
+      url='',
+      packages=find_packages(),
+     )
